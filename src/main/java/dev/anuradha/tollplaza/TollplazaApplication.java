7f6c9b766c01d7958432c6ac4da1b0ab182462f5
Anuradha Belgaonkar <annubelgaonkar@gmail.com>
2025-08-26 13:26:12 +0530

    Created project

---
+++ src/main/java/dev/anuradha/tollplaza/TollplazaApplication.java
@@ -0,0 +1,13 @@
+package dev.anuradha.tollplaza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TollplazaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TollplazaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/anuradha/tollplaza/TollplazaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.anuradha.tollplaza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TollplazaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
