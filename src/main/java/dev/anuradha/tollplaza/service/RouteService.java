0311fd5b3fa5b7aa18ce71cc78bcc6b76bc49426
Anuradha Belgaonkar <annubelgaonkar@gmail.com>
2025-08-30 23:14:00 +0530

    modified business logic to return proper exceptions

--- src/main/java/dev/anuradha/tollplaza/service/RouteService.java
+++ src/main/java/dev/anuradha/tollplaza/service/RouteService.java
@@ -1,5 +1,7 @@
 package dev.anuradha.tollplaza.service;
 
+import dev.anuradha.tollplaza.exception.InvalidPincodeException;
+import dev.anuradha.tollplaza.exception.SamePincodeException;
 import dev.anuradha.tollplaza.model.TollPlaza;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,9 +15,14 @@ public class RouteService {
     public double calculateDistance(String sourcePincode,
                                     String destinationPincode){
         if(sourcePincode.equals(destinationPincode)){
-            throw new IllegalArgumentException(
+            throw new SamePincodeException(
                     "Source and destination pincodes cannot be the same");
         }
+
+        if(!isValidPincode(sourcePincode) || !isValidPincode(destinationPincode)){
+            throw new InvalidPincodeException("Invalid source or destination pincode");
+        }
+
         return 200 + Math.random() * 1800;
     }
 
@@ -26,5 +33,8 @@ public class RouteService {
             String destinationPincode
     ){
         return allPlazas;
+    }
+    private boolean isValidPincode(String pincode){
+        return pincode != null && pincode.matches("\\d{6}");
     }
 }
